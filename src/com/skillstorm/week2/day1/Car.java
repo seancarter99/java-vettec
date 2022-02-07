2abc7ff7d7b363fcd8f7f1f4c09ede5140b67e44
seancarter99 <scarter@skillstorm.com>
2022-02-07 15:55:24 -0500

    Adding last minute updates

--- src/com/skillstorm/week2/day1/Car.java
+++ src/com/skillstorm/week2/day1/Car.java
@@ -11,12 +11,23 @@ public class Car {
 	int totalMileage = 0;
 	
 	public Car() {
-		super();
+		super(); // This calls the parent class's constructor (AKA Object)
 		year = 2000;
 		isNew = true;
 	}
 	
+	public Car(String model, String make, int year, String color) {
+		System.out.println("Constructor without boolean");
+		this.model = model;
+		this.make = make;
+		this.year = year;
+		this.color = color;
+		this.numSeats = 4;
+		this.isNew = true;
+	}
+	
 	public Car(String model, String make, int year, String color, int numSeats) {
+		System.out.println("Constructor without boolean");
 		this.model = model;
 		this.make = make;
 		this.year = year;
@@ -26,6 +37,7 @@ public class Car {
 	}
 	
 	public Car(String model, String make, int year, String color, int numSeats, boolean isNew) {
+		System.out.println("Constructor with boolean");
 		this.model = model;
 		this.make = make;
 		this.year = year;
@@ -35,9 +47,12 @@ public class Car {
 	}
 	
 	public void drive(int miles) {
-		this.isNew = false;
-		System.out.println("Driving " + miles + " miles!");
 		this.totalMileage += miles;
+		if (this.totalMileage >= 1000) {
+			this.isNew = false;
+		}
+		System.out.println("Driving " + miles + " miles!");
+		
 		System.out.println("The total mileage on the " + this.model + " is " + this.totalMileage + " miles!");
 	}
 	
@@ -50,8 +65,9 @@ public class Car {
 		System.out.println("Is the car new? " + modelT.isNew);
 		modelT.drive(50);
 		System.out.println("Is the car still new? " + modelT.isNew);
-		modelT.drive(100);
+		modelT.drive(1000);
+		System.out.println("Is the car still new? " + modelT.isNew);
 		peanutButter.drive(10);
-		System.out.println(modelT + " " + peanutButter);
+		System.out.println(modelT + " " + peanutButter); // Memory addresses
 	}
 }
