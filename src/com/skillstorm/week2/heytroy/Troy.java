f445ce413d40d02803b1c38a7a007454cb27cb50
seancarter99 <scarter@skillstorm.com>
2022-02-11 16:54:21 -0500

    Adding test folder

---
+++ src/com/skillstorm/week2/heytroy/Troy.java
@@ -0,0 +1,10 @@
+package com.skillstorm.week2.heytroy;
+
+public class Troy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Troy!");
+	}
+
+}
