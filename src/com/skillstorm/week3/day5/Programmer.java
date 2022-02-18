0d38de9e168c51a9308918dac226e48f897ef488
seancarter99 <scarter@skillstorm.com>
2022-02-18 14:01:17 -0500

    Adding the template code

---
+++ src/com/skillstorm/week3/day5/Programmer.java
@@ -0,0 +1,23 @@
+package com.skillstorm.week3.day5;
+
+import java.util.ArrayList;
+
+public class Programmer {
+
+	private String name;
+	private String company;
+	private ArrayList<String> programmingLanguages;
+	
+	// Insert Constructors here
+	
+	// Insert getters/setters here
+	
+	// Should also prevent null values from being added
+	public void learnLanguage(String programmingLanguage) {
+		// "Learns" or adds the programming language to the list
+	}
+	
+	public void forgetLanguage(String programmingLanguage) {
+		// "Forgets" or removes the programming language to the list
+	}
+}
