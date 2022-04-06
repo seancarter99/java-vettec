0c1b861233228efedd84741a27ff1a5d9213c701
seancarter99 <scarter@skillstorm.com>
2022-04-06 11:08:12 -0400

    Adding React Component Code

---
+++ src/com/skillstorm/week10/react-practice/src/App.jsx
@@ -0,0 +1,36 @@
+import { Header } from './components/Header.jsx'; // .js/.jsx is OPTIONAL
+
+// This works the same
+// function App() {
+
+// }
+
+// I prefer the arrow function
+const App = () => {
+    // In a "function component" we return all of the JSX that we need to render to the page
+    return (
+        // In React, you can only return 1 JSX element at a time
+
+        // This is a fragment and it does not render to the page
+        // It's useful for returning 2+ JSX elements
+        <>
+            <Header />
+            <Header />
+            <Header />
+            <p>
+                Today we are practicing React. React is a cool UI library.
+            </p>
+            
+        </>
+    );
+}
+
+export default App;
+
+// export
+// export default
+
+// You can only have one default export per file
+
+// Any regular export (no default) will be all bundled into one JS object
+// As such, importing them you'll use destructure syntax
---
+++ src/com/skillstorm/week10/react-practice/src/components/Header.jsx
@@ -0,0 +1,11 @@
+// This is a Header component
+// Whenever I use <Header /> it translates to <h1>Hello World</h1><h2>Made By: Sean Carter</h2>
+
+export const Header = () => {
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <h2>Made By: Sean Carter</h2>
+        </>
+    );
+}
